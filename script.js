8b92eab58943ed6c245586bde24e02bc6a6f6b24
Didiwolf <48806358+Didiwolf@users.noreply.github.com>
2019-03-21 20:57:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var question ="Ваш бюджет на месяц?";
+var question_2 ="ВВЕДИТЕ ДАТУ-YYYY.MM.DD";
+var question_3 ="Расходы в этом месяце на что?";
+var question_4 ="Во сколько обойдется??";
+var object ; {"money", "question_2"};
+var object_2 ; {}
+var money ; {"500","200","150"};
+prompt(question);
+    if(question == "ваш бюджет на месяц?");
+    prompt (question_2);
+    if (question_2 =="2019.03.21");
+    prompt (question_3);
+    if (question_3 == "жизнь");
+    prompt(question_4);
+    if(question_4 == "примерно 800");
+    alert("ваш бюджет на день:"+"1200"/30);
+
+    
+    
+
+
